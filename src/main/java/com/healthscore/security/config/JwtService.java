deadb67e47d45ae0471f37ade2a7d2903a3f4152
romanprotoliuk <romanprotoliuk@gmail.com>
2023-07-22 22:09:27 -0700

    auth controllers, register, and more

--- src/main/java/com/healthscore/security/config/JwtService.java
+++ src/main/java/com/healthscore/security/config/JwtService.java
@@ -8,6 +8,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.Data;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
@@ -44,6 +45,19 @@ public class JwtService {
                 .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24 ))
                 .signWith(getSignInKey(), SignatureAlgorithm.HS256)
                 .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
     }
 
     public Claims extractAllClaims(String token) {
